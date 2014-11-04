57d96d7ed48b922e76da78b6780aee59cebcb798
abramovdan@github.com <abramovdan@github.com>
2014-11-05 00:50:40 +0300

    first commit

---
+++ src/Models/Department.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class Department {
+	private long id;
+	private String name;
+	private Employee chief;
+	
+}
---
+++ src/Models/Employee.java
@@ -0,0 +1,49 @@
+package Models;
+
+public class Employee {
+	private long id;
+	private String name;
+	private Department department;
+	private String phoneNumber;
+	private double salary;
+	
+	public Employee(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public long getId() {
+		return id;
+	}
+}
